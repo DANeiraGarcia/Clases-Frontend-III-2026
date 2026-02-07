c43c90167f722fbb7f11478a4c2f7976e3ff837d
daniel alejandro neira <danielgrc85@gmail.com>
2026-02-06 19:57:53 -0500

    creacion del modulo de productos

---
+++ src/data/Products.js
@@ -0,0 +1,37 @@
+// se coloca el export para poder importar el arreglo en otros archivos
+export const products = [
+  {
+    id: 1,
+    name: "Laptop",
+    category: "Electronics",
+    price: 1200,
+    image: "https://images.pexels.com/photos/29765806/pexels-photo-29765806.jpeg",
+    description: "A high-performance laptop suitable for all your computing needs."
+  },
+  {
+    id: 2,
+    name: "Smartphone",
+    category: "Electronics",
+    price: 800,
+    image: "https://images.pexels.com/photos/11216260/pexels-photo-11216260.jpeg",
+    description: "A latest model smartphone with cutting-edge features."
+  },
+    {
+    id: 3,
+    name: "Headphones",
+    category: "Electronics",
+    price: 150,
+    image: "https://images.pexels.com/photos/3394650/pexels-photo-3394650.jpeg",
+    description: "Noise-cancelling headphones for an immersive audio experience."
+  },
+    {
+    id: 4,
+    name: "Tablet",
+    category: "Electronics",
+    price: 300,
+    image: "https://images.pexels.com/photos/6373045/pexels-photo-6373045.jpeg",
+    description: "A portable tablet for entertainment and productivity."
+  },
+]
+     
+    
