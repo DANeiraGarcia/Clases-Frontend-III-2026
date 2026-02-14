885156d758fa158ed0c18696af2448aba9be2844
daniel alejandro neira <danielgrc85@gmail.com>
2026-02-13 20:16:17 -0500

    agregar funcionalidad de likes con useState y eventos

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,14 +1,37 @@
+
+import styles from './ProductCard.module.css';
+import {useState} from 'react'
+
 function ProductCard({name,category,price,image,description}) {
+    const [likes, setLikes] = useState(0);
+    const [isLiked, setIsLiked] = useState(false);
+
+    const handleLike = () => {
+        if (isLiked) {
+            setLikes(likes - 1); // resta 1 y lo pone en falso y esto se llama hook
+            setIsLiked(false);
+        } else {
+            setLikes(likes + 1); // se controlan los estados de los likes y si el producto esta o no likeado
+            setIsLiked(true);
+        }
+    };
+    
+
     return (
-        <article className="product-card">
-            <img src={image} alt={name} className="product-image" />
-            <div className="product-info">
-                <span className="product-category">{category}</span>
-                <h3 className="product-name">{name}</h3>
-                <p className="product-description">{description}</p>
-                <div className="product-footer">
-                    <span className="product-price">${price.toFixed(2)}</span>
-                    <button className="btn-like">Me gusta</button>
+        <article className={styles.productCard}>
+            <img src={image} alt={name} className={styles.productImage} />
+            <div className={styles.productInfo}>
+                <span className={styles.productCategory}>{category}</span>
+                <h3 className={styles.productName}>{name}</h3>
+                <p className={styles.productDescription}>{description}</p>
+                <div className={styles.productFooter}>
+                    <span className={styles.productPrice}>${price.toFixed(2)}</span>
+                    <button 
+                    className={`${styles.btnLike} ${isLiked ? styles.liked : ''}`} 
+                    onClick={handleLike}
+                    >
+                        {isLiked ? '❤' : '🤍'} {likes} Me gusta
+                    </button>
                 </div>
             </div>
         </article>
--- src/page/ProductList.jsx
+++ src/page/ProductList.jsx
@@ -1,28 +1,30 @@
-import {products}from "../data/Products";
-import ProductCard from "../components/ProductCard"; // cuando tiene default el nomre del componente es el que se importa y no va en llaves
-import styles from "../components/ProductCard.module.css";
+import { products } from '../data/Products';
+import ProductCard from '../components/ProductCard'; // cuando tiene default el nomre del componente es el que se importa y no va en llaves
+import styles from '../components/ProductCard.module.css';
 
 function ProductList() {
-    return (
-        <div className={styles.container}>
-            <header className={styles.header}>
-                <h1 className={styles.title}>Productos Informaticos</h1>
-                <p className={styles.subtitle}>Explora nuestra selección de productos informáticos de alta calidad.</p>
-            </header>
-            <div className={styles.grid}>
-                {products.map((product) => (
-                    <ProductCard key={product.id}  // desde aca llamo el componente ProductCard y le paso las propiedades del producto
-                    name={product.name}
-                    category={product.category}
-                    price={product.price}
-                    image={product.image}
-                    description={product.description}
-                    />
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <h1 className={styles.title}>Productos Informaticos</h1>
+        <p className={styles.subtitle}>
+          Explora nuestra selección de productos informáticos de alta calidad.
+        </p>
+      </header>
+      <div className={styles.grid}>
+        {products.map((product) => (
+          <ProductCard
+            key={product.id} // desde aca llamo el componente ProductCard y le paso las propiedades del producto
+            name={product.name}
+            category={product.category}
+            price={product.price}
+            image={product.image}
+            description={product.description}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default ProductList;
-
