dc4a150c49af496502fd0e7c0dc39d7f0586abaf
daniel alejandro neira <danielgrc85@gmail.com>
2026-02-13 20:46:30 -0500

    feat: crear navbar con navegación y auth UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import ProductList from "./page/ProductList";
+import Home from "./page/Home";
+import Navbar from "./components/Navbar";
 import './App.css';
+import './components/Navbar.module.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <Home />
       <ProductList />
     </div>
   );
