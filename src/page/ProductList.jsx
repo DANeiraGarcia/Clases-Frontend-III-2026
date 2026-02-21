c0e2e452a8ad770912f9593f3dedda6cef8aab77
daniel alejandro neira <danielgrc85@gmail.com>
2026-02-20 21:05:06 -0500

    agregar producto al estado de productos

--- src/page/ProductList.jsx
+++ src/page/ProductList.jsx
@@ -2,36 +2,43 @@ import { products } from '../data/Products';
 import ProductCard from '../components/ProductCard'; // cuando tiene default el nomre del componente es el que se importa y no va en llaves
 import styles from '../components/ProductCard.module.css';
 import { useState } from "react";
+import ProductForm from '../components/ProductForm';
+
 
 
 
 
 function ProductList() {
-  const [productsState, setProductsState] = useState(products);
-  return (
-    
-    <div className={styles.container}>
-      <header className={styles.header}>
-        <h1 className={styles.title}>Productos Informaticos</h1>
-        <p className={styles.subtitle}>
-          Explora nuestra selección de productos informáticos de alta calidad.
-        </p>
-      </header>
-      <div className={styles.grid}>
-        {productsState.map((product) => (
-          <ProductCard
-            key={product.id} // desde aca llamo el componente ProductCard y le paso las propiedades del producto
-            name={product.name}
-            category={product.category}
-            price={product.price}
-            stock={product.stock}
-            image={product.image}
-            description={product.description}
-          />
-        ))}
-      </div>
-    </div>
-  );
-}
 
+    const [productsState, setProductsState] = useState(products);
+    const handleAddProduct = (product) => {
+        console.log("Producto recibido desde el form:", product);
+    };
+    
+    return (
+        <div className = {styles.container}>
+            <header className = {styles.header}>
+            <h1 className = {styles.title}>Lista de Productos</h1>
+            <p className = {styles.description}>Explora nuestra selección de productos!</p>
+            </header>
+
+            <ProductForm onSubmit={handleAddProduct} />
+
+            <div className = {styles.grid}>
+            {productsState.map(product => (
+                <ProductCard
+                key={product.id}
+                name={product.name}
+                category={product.category}
+                stock={product.stock}
+                price={product.price}
+                image={product.image}
+                description={product.description}
+                />
+            ))}
+            </div>
+        </div>
+    )          
+}
+ 
 export default ProductList;
