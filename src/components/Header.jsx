81960e1298da79483ee5b72b664e7fb0f63f919f
daniel alejandro neira <danielgrc85@gmail.com>
2026-04-16 17:02:39 -0500

    refractor Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,11 @@
-import styles from "./styles/Header.module.css";
-import Navbar from "./Navbar";
+import styles from '../styles/Header.module.css';
 
-function Header({ activePage, onNavigate, user, onSignIn, onSignOut, cartItemCount }) {
+import Navbar from './Navbar';
+
+function Header({ user, onSignOut, cartItemCount }) {
   return (
     <header className={styles.header}>
-      <Navbar
-        activePage={activePage}
-        onNavigate={onNavigate}
-        user={user}
-        onSignIn={onSignIn}
-        onSignOut={onSignOut}
-        cartItemCount={cartItemCount}
-      />
+      <Navbar user={user} onSignOut={onSignOut} cartItemCount={cartItemCount} />
     </header>
   );
 }
